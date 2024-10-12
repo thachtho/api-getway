d8e9c461408b9310ab7f7828644f0f3bff1f9e12
thachtho <buithanhtho15ig@gmail.com>
2024-10-12 18:58:37 +0700

    [feat] get users

--- src/app/controllers/res-api/users/users.controller.ts
+++ src/app/controllers/res-api/users/users.controller.ts
@@ -1,8 +1,8 @@
 import { Body, Controller, Get, Post, Put, Query } from '@nestjs/common';
+import { IUser } from '../../../common/interface/user.i';
 import { UsersService } from '../../../infrastructure/services/users/users.service';
 import { Route } from '../rest-api.i';
-import { TypeCreateDto } from './user.service.i';
-import { IUser } from '../../../common/interface/user.i';
+import { TypeCreateDto, TypeGetUserDto } from './user.service.i';
 
 @Controller(Route.USERS)
 export class UsersController {
@@ -11,6 +11,12 @@ export class UsersController {
   @Get('get-role')
   getRole() {
     return this.usersService.getRole();
+  }
+
+  @Get('')
+  findAll(@Query() query: TypeGetUserDto) {
+    const { typeGet } = query;
+    return this.usersService.findAll(Number(typeGet));
   }
 
   @Post('')
--- src/app/infrastructure/services/users/user.service.i.ts
+++ src/app/infrastructure/services/users/user.service.i.ts
@@ -1,5 +1,6 @@
 import { IUser } from '../../../common/interface/user.i';
 
-export type TypeCreateUser = 'adminAgency' | 'teacher' | 'student';
+export type TypeUser = 'adminAgency' | 'teacher' | 'student';
+export type TypeGetUser = 1 | 2 | 3 | 4;
 
 export interface UserArg extends IUser {}
